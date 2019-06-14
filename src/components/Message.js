7fc6f677899b3e875b046b08a3484e9e4c6f8aa3
Arun <s.arunsubramaniam@gmail.com>
2019-06-15 00:46:03 +0530

    Initial commit

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Message extends React.Component {
+    render(){
+        return (
+            <div className="message">
+                message list
+            </div>
+        );
+    }
+}
+
+export default Message
